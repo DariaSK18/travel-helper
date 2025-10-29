f35fc28500abbb671c17fd7229e9d4f08e85f818
Daria Steblovska <darias1896.96@gmail.com>
2025-10-29 23:33:36 +0000

    command start added with btn

--- main.py
+++ main.py
@@ -10,3 +10,13 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(BOT_TOKEN)
 
 # handling /start command
+@bot.message_handler(commands=['start'])
+def main(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('Get currency exchange', callback_data='currency'))
+    bot.send_message(message.chat.id, f'Hello, {message.from_user.first_name}!\nWhat country would you like to explore?', reply_markup=markup)
+
+def get_currency():
+    pass
+
+bot.polling(non_stop=True)
