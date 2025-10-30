5e677404f7eef60c1fd7af4376046ccb043fbe8b
Daria Steblovska <darias1896.96@gmail.com>
2025-10-30 03:52:47 +0000

    other currency pairs func implemented

--- main.py
+++ main.py
@@ -15,11 +15,13 @@ WEATHER_API = os.getenv("WEATHER_API")
 bot = telebot.TeleBot(BOT_TOKEN)
 currency_converter = CurrencyConverter()
 
-amount = 0
+user_amounts = {}
+user_states = {}
 
 # handling /start command
 @bot.message_handler(commands=['start'])
 def main(message):
+    user_states[message.chat.id] = None
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton('Get currency exchange', callback_data='currency'))
     markup.add(types.InlineKeyboardButton('Get country information', callback_data='country'))
@@ -27,12 +29,24 @@ def main(message):
 
 @bot.callback_query_handler(func=lambda call: call.data == 'country')
 def country(call):
+    user_states[call.message.chat.id] = 'awaiting_country'
     bot.send_message(call.message.chat.id,'What country would you like to explore?')
 
 @bot.message_handler(content_types=['text'])
+def handle_text(message):
+    state = user_states.get(message.chat.id)
+    if state == 'awaiting_amount':
+        summa(message)
+    elif state == 'awaiting_country':
+        get_country(message)
+    elif state == 'awaiting_currency_pair':
+        user_currency(message)
+    else:
+        bot.send_message(message.chat.id, 'Please select an operation first (/start).')
+
 def get_country(message):
-    country = message.text.strip().lower()
-    res = requests.get(f'https://restcountries.com/v3.1/name/{country}')
+    country_title = message.text.strip().lower()
+    res = requests.get(f'https://restcountries.com/v3.1/name/{country_title}')
     print(res)
     if res.status_code == 200:
         data_list = res.json()
@@ -75,14 +89,13 @@ def get_temperature(city):
 
 def summa(message):
     try:
-        global amount
-        amount = float(message.text.strip())
+        user_amounts[message.chat.id] = float(message.text.strip())
     except ValueError:
         bot.send_message(message.chat.id, 'Invalid format, please enter a number.')
         bot.register_next_step_handler(message, summa)
         return
 
-    if amount > 0:
+    if user_amounts[message.chat.id] > 0:
         markup = types.InlineKeyboardMarkup(row_width=2)
         btn1 = types.InlineKeyboardButton('USD to EUR', callback_data='usd/eur')
         btn2 = types.InlineKeyboardButton('EUR to USD', callback_data='eur/usd')
@@ -93,6 +106,7 @@ def summa(message):
     else:
         bot.send_message(message.chat.id, 'Amount must be greater than 0.')
         bot.register_next_step_handler(message, summa)
+        return
 
 # def get_places():
 #
@@ -133,18 +147,46 @@ def summa(message):
 
 @bot.callback_query_handler(func=lambda call: call.data == 'currency')
 def get_currency(call):
+    user_states[call.message.chat.id] = 'awaiting_amount'
     msg = bot.send_message(call.message.chat.id, 'Enter amount, please.')
     bot.register_next_step_handler(msg, summa)
 
 @bot.callback_query_handler(func=lambda call: call.data in ['usd/eur', 'eur/usd', 'usd/gbp', 'other'])
 def exchange(call):
-    values = call.data.upper().split('/')
-    res = round(currency_converter.convert(amount, values[0], values[1]), 2)
-    text = (
-        f'Result: {res}.\n'
-        f'You can re-enter different amount.'
-    )
-    bot.send_message(call.message.chat.id, text)
-    bot.register_next_step_handler(call.message, summa)
+    # user_states[call.message.chat.id] = 'awaiting_currency_pair'
+    if call.data != 'other':
+        amount = user_amounts.get(call.message.chat.id, 0)
+        values = call.data.upper().split('/')
+        try:
+            res = round(currency_converter.convert(amount, values[0], values[1]), 2)
+            text = (
+                f'Result: {res}.\n'
+                f'You can re-enter different amount.'
+            )
+        except Exception:
+            text = 'Conversion failed. Try again.'
+        bot.send_message(call.message.chat.id, text)
+        user_states[call.message.chat.id] = None
+        # bot.register_next_step_handler(call.message, summa)
+    else:
+        user_states[call.message.chat.id] = 'awaiting_currency_pair'
+        bot.send_message(call.message.chat.id, 'Enter your currency pair separated by a slash')
+        # bot.register_next_step_handler(call.message, user_currency)
+
+def user_currency(message):
+    try:
+        values = message.text.upper().split('/')
+        amount = user_amounts.get(message.chat.id, 0)
+        res = round(currency_converter.convert(amount, values[0], values[1]), 2)
+        text = (
+            f'Result: {res}.\n'
+            f'You can re-enter different amount.'
+        )
+        bot.send_message(message.chat.id, text)
+        # bot.register_next_step_handler(message, summa)
+    except Exception:
+        bot.send_message(message.chat.id, 'Conversion failed. Try again.')
+        # bot.register_next_step_handler(message, summa)
+        user_states[message.chat.id] = None
 
 bot.polling(non_stop=True)
