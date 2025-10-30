fd50f2c4b000a462b05c357049cdd36a8a3c1a0a
Daria Steblovska <darias1896.96@gmail.com>
2025-10-30 02:04:29 +0000

    btn added for currency exchange and message reply

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ from telebot import types
 from dotenv import load_dotenv
 import os
 import requests
+from currency_converter import CurrencyConverter
 
 # load bot token
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 WEATHER_API = os.getenv("WEATHER_API")
+# CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+# CLIENT_ID = os.getenv("CLIENT_ID")
 
 bot = telebot.TeleBot(BOT_TOKEN)
+currency_converter = CurrencyConverter()
 
 # handling /start command
 @bot.message_handler(commands=['start'])
 def main(message):
-    markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton('Get currency exchange', callback_data='currency'))
-    bot.send_message(message.chat.id, f'Hello, {message.from_user.first_name}!\nWhat country would you like to explore?', reply_markup=markup)
+    # markup = types.InlineKeyboardMarkup()
+    # markup.add(types.InlineKeyboardButton('Get currency exchange', callback_data='currency'))
+    bot.send_message(message.chat.id, f'Hello, {message.from_user.first_name}!\nWhat country would you like to explore?')
 
 @bot.message_handler(content_types=['text'])
 def get_country(message):
@@ -36,6 +40,8 @@ def get_country(message):
         languages = ', '.join(data.get('languages', {}).values())
         temperature = get_temperature(capital)
 
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton('Get currency exchange', callback_data='currency'))
         text = (
             f'{flag} *{name}*\n'
             f'Capital: {capital}\n'
@@ -46,7 +52,7 @@ def get_country(message):
             f'Temperature: {temperature} °C\n'
             f'Map: {maps}'
         )
-        bot.reply_to(message, text, parse_mode='Markdown')
+        bot.reply_to(message, text, parse_mode='Markdown', reply_markup=markup)
     else:
         bot.reply_to(message, 'Country not found.')
 
@@ -60,7 +66,45 @@ def get_temperature(city):
     else:
         return "Sorry, temperature info is currently unavailable"
 
-def get_currency():
-    pass
+# def get_places():
+#
+#     token_res = requests.post(
+#         "https://test.api.amadeus.com/v1/security/oauth2/token",
+#         data={
+#             "grant_type": "client_credentials",
+#             "client_id": CLIENT_ID,
+#             "client_secret": CLIENT_SECRET
+#         }
+#     )
+#     token_res.raise_for_status()
+#     access_token = token_res.json()["access_token"]
+#
+#     # Запрос к Tours & Activities
+#     latitude = 41.397158  # пример — можно заменить на координаты города
+#     longitude = 2.160873
+#     radius = 5000  # радиус в метрах
+#
+#     url = "https://test.api.amadeus.com/v1/shopping/activities"
+#     headers = {
+#         "Authorization": f"Bearer {access_token}"
+#     }
+#     params = {
+#         "latitude": latitude,
+#         "longitude": longitude,
+#         "radius": radius
+#     }
+#     res = requests.get(url, headers=headers, params=params)
+#     res.raise_for_status()  # добавим проверку на ошибки
+#     data = res.json()
+#     for activity in data.get("data", [])[:3]:
+#         name = activity.get("name")
+#         price_info = activity.get("price", {})
+#         amount = price_info.get("amount")
+#         currency = price_info.get("currency")
+#         print(f"Name: {name}, Price: {amount} {currency}")
+
+@bot.callback_query_handler(func=lambda call: call.data == 'currency')
+def get_currency(call):
+    bot.send_message(call.message.chat.id, 'Enter amount, please.')
 
 bot.polling(non_stop=True)
